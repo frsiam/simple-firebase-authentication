2383047c1b8733aab472230b73d0cf541f2fa9f1
frsiam <siam.frs@gmail.com>
2022-04-06 15:27:54 +0600

    include the github sign in popup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,62 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
   const [user, setUser] = useState({})
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider(); 
 
-  const handleGoogleSignIn = () =>{
-    
-    signInWithPopup(auth, provider)
+  const handleGoogleSignIn = () => {
+
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user)
+        console.log(user);
+      })
+      .catch(error => {
+        console.error('error', error)
+      })
+  }
+
+  const handleGithubSignIn = () => {
+
+    signInWithPopup(auth, githubProvider)
     .then(result => {
       const user = result.user;
       setUser(user)
       console.log(user);
     })
     .catch(error => {
-      console.error('error', error)
+      console.error(error);
     })
   }
 
   const handleGoogleSignOut = () => {
     signOut(auth)
-    .then( () => {
-      setUser({})
-    })
-    .catch(error => {
-      setUser({})
-    })
+      .then(() => {
+        setUser({})
+      })
+      .catch(error => {
+        setUser({})
+      })
   }
 
   return (
     <div className="App">
 
       {
-        user.email ? 
-        <button onClick={handleGoogleSignOut}>Sign Out</button> 
-        :
-        <button onClick={handleGoogleSignIn}>Google Sign-in</button>
+        user.displayName ?
+          <button onClick={handleGoogleSignOut}>Sign Out</button>
+          :
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sign-In</button>
+            <button onClick={handleGithubSignIn}>Github Sign-In</button>
+          </>
       }
 
       <h3>Name: {user.displayName}</h3>
